51b10e2d917bfb39af253b425241f92f23076b68
TranTin <trantin23012001@gmail.com>
2024-11-12 10:22:50 +0700

    3th

--- Task_scheduler/Src/main.c
+++ Task_scheduler/Src/main.c
@@ -35,7 +35,13 @@ void task4_handler(void);
 
 #define SRAM_START			0x20000000U
 #define SIZE_SRAM			((128)*(1024))
+#define SRAM_END			((SRAM_START)+(SIZE_SRAM))
 
+#define T1_STACK_START		SRAM_END
+#define T2_STACK_START		((SRAM_END)-(1*SIZE_TASK_STACK))
+#define T3_STACK_START		((SRAM_END)-(2*SIZE_TASK_STACK))
+#define T4_STACK_START		((SRAM_END)-(3*SIZE_TASK_STACK))
+#define SCHED_STACK_START	((SRAM_END)-(4*SIZE_TASK_STACK))
 
 int main(void)
 {
